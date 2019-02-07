bc70163b7ef7ad5b0c997ddb0f8b38aa836657ec
Jaclyn Chin <jac289@ucsd.edu>
2019-02-07 00:14:23 -0800

    new message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import bob from "./bob";
+import cathy from "./cathy";
+import sam from "./sam";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <HashRouter>
+ <div>
+          <ul className="header">
+          <h1>Home</h1>
+          </ul>
+
+<ul className = "pretty">
+          <table>
+  <tr>
+    <th>Messages</th>
+  </tr>
+  <tr>
+    <td> <button class="button button4"> <li><NavLink to ="/Bob">Bob</NavLink></li></button></td>
+
+  </tr>
+  <tr>
+    <td><button class="button button4"><li><NavLink to ="/Cathy">Cathy</NavLink></li></button></td>
+
+  </tr>
+  <tr>
+    <td><button class="button button4"><li><NavLink to="/Sam">Sam</NavLink></li></button></td>
+</tr>
+
+</table>
+</ul>
+          <div className="content">
+          <Route exact path ="/bob" component = {bob}/>
+           <Route path="/cathy" component={cathy}/>
+            <Route path="/sam" component={sam}/>
+             
+          </div>
+        </div>
+        </HashRouter>
     );
   }
 }
---
+++ src/sam.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class sam extends Component {
+  render() {
+    return (
+      <div>
+        <h2>SAM?</h2>
+        <p>The easiest thing to do is post on
+        our <a href="http://forum.kirupa.com">forums</a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default sam;
